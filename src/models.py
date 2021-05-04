07b158baba34f9ad882f6007805682f136c2c57a
Mykega <m.garcia13_3@hotmail.com>
2021-05-04 22:41:24 +0000

    correccion

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String, Enum
+from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,51 +8,45 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-
 class User(Base):
     __tablename__ = 'user'
-    # Here we define columns for the table USER.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True, nullable=False)
-    username = Column(String(250))
-    firstname = Column(String(250))
-    lastname = Column(String(250))
-    email = Column(String(250))
-
-class Follower(Base):
-    __tablename__ = 'follower'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key = True)
-    user_from_id = Column(Integer, ForeignKey('user.id'),primary_key=True)
-    user_to_id = Column(Integer, ForeignKey('user.id'))
+    user_id = Column(Integer, primary_key=True)
+    username = Column(String(80), nullable=False, unique=True)
+    firstname = Column(String(80), nullable=False)
+    lastname = Column(String(80), nullable=False)
+    email = Column(String(250), nullable=False, unique=True)
+    profile_url = Column(String(250), nullable=False)
+    created_at = Column(String(250))
+    updated_at = Column(String(250))
 
 class Post(Base):
     __tablename__ = 'post'
-    id = Column(Integer, primary_key = True)
-    user_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.user_id'))
+    type_post = Column(String(250))
+    comment = Column(String(250))
+    ubication = Column(String(250))
+    created_at = Column(String(250))
+    updated_at = Column(String(250))
+    total_likes = Column(Integer)
 
-class Media(Base):
-    __tablename__ = 'media'
-    # Here we define columns for the table USER.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key= True, nullable=False)
-    tipe = Column(Enum())
-    url = Column(String(250))
-    post_id = Column(Integer, ForeignKey("post.id"))
-
-class Comment(Base):
-    __tablename__ = "comment"
-    id = Column(Integer, primary_key=True)
+class PostComment(Base):
+    __tablename__ = 'postComment'
+    comment_id = Column(Integer, primary_key=True)
+    author_comment_id = Column(Integer, ForeignKey('user.user_id'))
     comment_text = Column(String(250))
-    author_id = Column(Integer, ForeignKey("user.id"))
-    post_id = Column(Integer, ForeignKey("post.id"))
+    post_id = Column(Integer, ForeignKey('post.post_id'))
     
-    
+
+class Likes(Base):
+    __tablename__ = 'likes'
+    like_id = Column(Integer, primary_key=True)
+    post_id = Column(Integer, ForeignKey('post.post_id'))
+    user_likeit_id = Column(Integer, ForeignKey('user.user_id'))
+    time = Column(String(250))
 
     def to_dict(self):
         return {}
-        
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
